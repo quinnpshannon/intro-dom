a7a8bf958f91a8fcff4810591be270fe045dce53
Quinn Shannon <quinn.p.shannon@gmail.com>
2024-04-16 16:02:30 -0400

    Notes for Today

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const ul = document.getElementById('todo-list'); //IDs only
+// const h1 = document.getElementById('heading-1');
+
+const h1 = document.querySelector('#heading-1'); // id
+const navbar = document.querySelector('.navbar'); // class
+const h2 = document.querySelector('h2'); // element name
+
+const lis = document.querySelectorAll('li'); // element name
+
+console.log(h1.innerText);
+console.log(ul);
+console.log(navbar);
+console.log(h2);
+console.log(lis);
